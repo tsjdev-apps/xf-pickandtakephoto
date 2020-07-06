b0366bbf9d05ac9935ad3a78959c6f6088b5ed01
Sebastian Jensen <apps@tsjdev-apps.de>
2020-07-06 21:27:04 +0200

    - add basic solution and logic to pick/take a photo

---
+++ XFPickAndTakePhoto/XFPickAndTakePhoto/XFPickAndTakePhoto/MainPage.xaml.cs
@@ -0,0 +1,55 @@
+﻿using Plugin.Media;
+using Plugin.Media.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XFPickAndTakePhoto
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void PickPhotoButtonOnClicked(object sender, EventArgs e)
+        {
+            // initialie
+            await CrossMedia.Current.Initialize();
+
+            // pick photo
+            var mediaFile = await CrossMedia.Current.PickPhotoAsync(new PickMediaOptions { PhotoSize = PhotoSize.Medium, CompressionQuality = 90 });
+            if (mediaFile == null)
+                return;
+
+            // show image
+            PickedImage.Source = ImageSource.FromStream(() => mediaFile.GetStream());
+        }
+
+        private async void TakePhotoButtonOnClicked(object sender, EventArgs e)
+        {
+            // initialize
+            await CrossMedia.Current.Initialize();
+
+            // check if camera is available
+            if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
+                return;
+
+            // take photo
+            var mediaFile = await CrossMedia.Current.TakePhotoAsync(new StoreCameraMediaOptions { Directory = "XFPickAndTakePhoto", Name = $"{Guid.NewGuid()}.jpg", SaveToAlbum = true, SaveMetaData = true });
+            if (mediaFile == null)
+                return;
+
+            // show image
+            PickedImage.Source = ImageSource.FromStream(() => mediaFile.GetStream());
+        }
+    }
+}
